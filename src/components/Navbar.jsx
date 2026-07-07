c58f369d0ca519a48db556ff194273b8de8780c1
Olatunji <olatee54@gmail.com>
2026-07-07 12:17:26 +0100

    Add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>ChainUI</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>Docs</li>
+      </ul>
+    </nav>
+  );
+}
